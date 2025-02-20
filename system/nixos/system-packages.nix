266754baf1e8a3ee6d148fe0eafa73401833c8bd
TsFreddie NixOS <whatis+nix@tsdo.in>
2025-02-20 15:08:39 +0800

    system generation 101

--- system/nixos/system-packages.nix
+++ system/nixos/system-packages.nix
@@ -18,7 +18,31 @@
     xorg.xrdb
 
     kdePackages.partitionmanager
-    kdePackages.kcalc
+    kdePackages.kdenlive
+    kdePackages.filelight
+    kdePackages.kolourpaint
+    kdePackages.kfind
+    kdePackages.kcolorchooser
+    kdePackages.kcolorpicker
+    kdePackages.ksystemlog
+    kdePackages.krdc
+    kdePackages.kgpg
+    kdePackages.kompare
+    kdePackages.k3b
+    kdePackages.skanlite
+    kdePackages.kruler
+    kdePackages.kleopatra
+    kdePackages.ktorrent
+    kdePackages.kget
+    kdePackages.lokalize
+    kdePackages.kclock
+    kdePackages.kmail
+    kdePackages.kontact
+    kdePackages.merkuro
+
+    kdePackages.audiotube
+
+    qalculate-qt
 
     stable.bitwarden-desktop
     stable.bitwarden-cli
