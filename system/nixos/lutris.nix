5b85f0cf6b5c7cd0e8b2a6beae8881dc5337486d
NixOS Generation <not-valid@nixos.com>
2025-03-03 11:50:40 +0800

    system generation 133

---
+++ system/nixos/lutris.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    (lutris.override {
+      extraLibraries = pkgs: [
+        # List library dependencies here
+      ];
+    })
+  ];
+}
