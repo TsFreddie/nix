5d039e58688df70d6431af64bb5d1a39955b4362
NixOS Generation <not-valid@nixos.com>
2025-03-09 16:00:02 +0800

    system generation 142

--- system/nixos/system-packages.nix
+++ system/nixos/system-packages.nix
@@ -1,52 +1,59 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    wget
-    vscode
-    nixd
-    nixfmt-rfc-style
-    thunderbird
-    gamescope
-    mpv 
-    vial
-    btop-rocm
+  environment.systemPackages =
+    with pkgs;
+    [
+      git
+      vim
+      wget
+      vscode
+      nixd
+      nixfmt-rfc-style
+      thunderbird
+      gamescope
+      mpv
+      vial
+      btop-rocm
 
-    xsettingsd
-    xorg.xrdb
+      xsettingsd
+      xorg.xrdb
 
-    kdePackages.partitionmanager
-    kdePackages.kdenlive
-    kdePackages.filelight
-    kdePackages.kfind
-    kdePackages.kcolorchooser
-    kdePackages.kcolorpicker
-    kdePackages.ksystemlog
-    kdePackages.krdc
-    kdePackages.kgpg
-    kdePackages.kompare
-    kdePackages.k3b
-    kdePackages.skanlite
-    kdePackages.kruler
-    kdePackages.kleopatra
-    kdePackages.ktorrent
-    kdePackages.lokalize
-    kdePackages.kclock
-    kdePackages.kmail
-    kdePackages.kontact
-    kdePackages.merkuro
+      kdePackages.partitionmanager
+      kdePackages.kdenlive
+      kdePackages.filelight
+      kdePackages.kfind
+      kdePackages.kcolorchooser
+      kdePackages.kcolorpicker
+      kdePackages.ksystemlog
+      kdePackages.krdc
+      kdePackages.kgpg
+      kdePackages.kompare
+      kdePackages.k3b
+      kdePackages.skanlite
+      kdePackages.kruler
+      kdePackages.kleopatra
+      kdePackages.ktorrent
+      kdePackages.lokalize
+      kdePackages.kclock
+      kdePackages.kmail
+      kdePackages.kontact
+      kdePackages.merkuro
 
-    qalculate-qt
+      qalculate-qt
 
-    ghostty
-    gearlever
+      ghostty
+      gearlever
 
-    stable.bitwarden-desktop
-    stable.bitwarden-cli
-    stable.chromium
-  ];
+      stable.bitwarden-desktop
+      stable.bitwarden-cli
+      stable.chromium
+    ]
+    ++ [
+      # fixes gnome/gtk stuff
+      adwaita-icon-theme
+      adwaita-icon-theme-legacy
+    ];
 
   services.udev.packages = with pkgs; [
     vial
