1d30ca1b9550e2d34ab9c27509f64752e5a19cd2
NixOS Generation <not-valid@nixos.com>
2025-04-07 23:24:59 +0800

    system generation 188

--- system/packages/ugreenlink/default.nix
+++ system/packages/ugreenlink/default.nix
@@ -52,7 +52,7 @@ stdenv.mkDerivation rec {
 
     for i in 16 24 48 64 96 128 256 512; do
       mkdir -p $out/share/icons/hicolor/''${i}x''${i}/apps
-      convert -background none -resize ''${i}x''${i} $out/pkgs/icon.png $out/share/icons/hicolor/''${i}x''${i}/apps/${pname}.png
+      magick convert -background none -resize ''${i}x''${i} $out/pkgs/icon.png $out/share/icons/hicolor/''${i}x''${i}/apps/${pname}.png
     done
 
     install -Dm644 ${desktopItem}/share/applications/${pname}.desktop -t $out/share/applications
