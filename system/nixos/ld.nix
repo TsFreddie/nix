57e8846367157b0a80ce4ca77d3999d1e56e614f
NixOS Generation <not-valid@nixos.com>
2025-09-10 15:58:17 +0800

    system generation

--- system/nixos/ld.nix
+++ system/nixos/ld.nix
@@ -3,6 +3,10 @@
 {
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
+    # for dotnet
     icu
+
+    # for some native lib on javascript projects
+    stdenv.cc.cc.lib
   ];
 }
