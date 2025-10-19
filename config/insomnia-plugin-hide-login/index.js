a7e73b1afb24dd077a87479b4c0baaa7decc0271
TsFreddie <whatis@tsdo.in>
2025-10-19 13:46:20 +0800

    insomnia plugin update

--- config/insomnia-plugin-hide-login/index.js
+++ config/insomnia-plugin-hide-login/index.js
@@ -2,36 +2,36 @@ const sheet = new CSSStyleSheet();
 sheet.id = 'insomnia-plugin-hide-login';
 sheet.replaceSync(`
   /* Clean-up Header */
-  #root .\\[grid-area\\:Header\\] div:has( > span > a[href='https://github.com/Kong/insomnia/stargazers']),
-  #root .\\[grid-area\\:Header\\] div > a[href='/auth/login'] {
+  .\\[grid-area\\:Header\\] div:has( > span > a[href='https://github.com/Kong/insomnia/stargazers']),
+  .\\[grid-area\\:Header\\] div > a[href='/auth/login'] {
     display: none !important;
   }
   
   /* Clean-up Navbar */
-  #root .grid-template-app-layout.with-navbar {
+  .grid-template-app-layout.with-navbar {
     grid-template-areas:
       "Header Header"
       "Content Content"
       "Statusbar Statusbar" !important;
   }
-  #root .\\[grid-area\\:Navbar\\] {
+  .\\[grid-area\\:Navbar\\] {
     display: none !important;
   }
   
   /* Clean-up Sidebar */
-  #root #sidebar > div > div:first-child > ol > li:has([data-testid='project']) {
+  #sidebar > div > div:first-child > ol > li:has([data-testid='project']) {
     display: none !important;
   }
       
   /* Clean-up Statusbar */
-  #root .\\[grid-area\\:Statusbar\\] > button,
-  #root .\\[grid-area\\:Statusbar\\] > div:last-child > div button:not([data-testid])  {
+  .\\[grid-area\\:Statusbar\\] > button,
+  .\\[grid-area\\:Statusbar\\] > div:last-child > div button:not([data-testid])  {
     display: none !important;
   }
-  #root .\\[grid-area\\:Statusbar\\] > div:last-child > div a  {
+  .\\[grid-area\\:Statusbar\\] > div:last-child > div a  {
     display: none !important;
   }
-  #root .\\[grid-area\\:Statusbar\\] > div:first-child > button:first-child {
+  .\\[grid-area\\:Statusbar\\] > div:first-child > button:first-child {
     display: none !important;
   }
 `);
